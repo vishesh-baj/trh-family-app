04f6f26caaca5ec2ec88cb0b78474d17b45a2c6d
vishesh-baj <vishesh.bajpayee@icloud.com>
2022-04-26 18:23:13 +0530

    added userCard for specific user info and made it dynamic

---
+++ src/components/SubComponents/UserInfoCard.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+const UserInfoCard = ({ user }) => {
+  return (
+    <React.Fragment>
+      <div className="max-w-5xl px-8 py-4 mx-auto mt-16 bg-white rounded-lg shadow-lg h-screen dark:bg-gray-800">
+        <div className="flex justify-center -mt-16 md:justify-end">
+          <img
+            className="object-cover w-20 h-20 border-2 border-blue-500 rounded-full dark:border-blue-400"
+            alt="Testimonial avatar"
+            src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=76&q=80"
+          />
+        </div>
+
+        <h2 className="mt-2 text-2xl font-semibold text-gray-800 dark:text-white md:mt-0 md:text-3xl">
+          User Details
+        </h2>
+
+        <div className="flex gap-10 justify-between w-1/2 ">
+          <p className="mt-2 font-semibold text-gray-600 dark:text-gray-200">
+            Full Name :
+          </p>
+          <p className="mt-2 text-gray-600 dark:text-gray-200">
+            ${user.first_name} ${user.last_name}
+          </p>
+        </div>
+
+        <div className="flex justify-between gap-10 w-1/2">
+          <p className="mt-2 font-semibold text-gray-600 dark:text-gray-200">
+            Role :
+          </p>
+
+          <p className="mt-2 text-gray-600 dark:text-gray-200">{user.role}</p>
+        </div>
+
+        <div className="flex justify-between gap-10 w-1/2">
+          <p className="mt-2 font-semibold text-gray-600 dark:text-gray-200">
+            Contact Number :
+          </p>
+
+          <p className="mt-2 text-gray-600 dark:text-gray-200">
+            {user.contact_no}
+          </p>
+        </div>
+
+        <div className="flex justify-end mt-4">
+          <a
+            href="#"
+            className="text-xl font-medium text-blue-500 dark:text-blue-300"
+          >
+            Edit Details
+          </a>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default UserInfoCard;
--- src/pages/UserDetails.jsx
+++ src/pages/UserDetails.jsx
@@ -1,24 +1,38 @@
 import React from "react";
 import { useSelector } from "react-redux";
-
+import UserInfoCard from "../components/SubComponents/UserInfoCard";
 const UserDetails = () => {
   const user = useSelector((state) => state.selectedUser);
 
   return (
     <React.Fragment>
-      <div className="mt-20 container mx-auto">
-        <h1 className="">FirstName:</h1>
-        <h1>{user.first_name}</h1>
+      <div className="mt-36">
+        <UserInfoCard user={user} />
       </div>
-      <h1>
-        LastName: <span>{user.last_name}</span>
-      </h1>
-      <h1>
-        Role: <span>{user.role}</span>
-      </h1>
-      <h1>
-        Contact Number: <span>{user.contact_no}</span>
-      </h1>
+
+      {/* <div className="container mt-28 mx-auto flex flex-col gap-5 justify-start px-5 ">
+        <div className=" px-5 py-2 w-auto rounded-lg bg-gray-50 shadow-md flex gap-10  ">
+          <h1 className="text-2xl">First Name: </h1>
+          <h1 className="text-2xl">{user.first_name}</h1>
+        </div>
+
+        <div className=" px-5 py-2 w-auto rounded-lg bg-gray-50 shadow-md flex gap-10  ">
+          <h1 className="text-2xl">Last Name: </h1>
+          <h1 className="text-2xl">{user.last_name}</h1>
+        </div>
+
+        <div className=" px-5 py-2 w-auto rounded-lg bg-gray-50 shadow-md flex gap-10  ">
+          <h1 className="text-2xl">Role: </h1>
+          <h1 className="text-2xl">{user.role}</h1>
+        </div>
+
+        <div className=" px-5 py-2 w-auto rounded-lg bg-gray-50 shadow-md flex gap-10  ">
+          <h1 className="text-2xl">Contact: </h1>
+          <h1 className="text-2xl">{user.contact_no}</h1>
+        </div>
+      </div> */}
+
+      <div className="container mx-auto flex"></div>
     </React.Fragment>
   );
 };
