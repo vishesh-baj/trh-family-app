29f5eec5bad06a8ed86571e74678882389703485
vishesh-baj <vishesh.bajpayee@icloud.com>
2022-05-09 13:54:03 +0530

    added get request to dashboard instead of landing page login handle

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Outlet } from "react-router-dom";
+import { getAllEmployees } from "../app/api";
+import { checkLocalStorage, filterEmployees } from "../app/helpers";
 import DashboardView from "../components/DashboardView";
 import Sidebar from "../components/Sidebar";
+import { getUsers } from "../features/user/UserDataSlice";
+
 const Dashboard = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const dashboardInit = async () => {
+      const employeesList = await getAllEmployees();
+      const filteredEmployeeObj = filterEmployees(employeesList);
+      // * Checking if localstorage token is present, and then only dispatching the filtered array
+      checkLocalStorage() && dispatch(getUsers(filteredEmployeeObj));
+    };
+    dashboardInit();
+  }, [dispatch]);
+
   return (
     <div className="flex mt-14 ">
       <Sidebar />
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { login } from "../app/api";
-import { checkLocalStorage } from "../app/helpers";
-import { filterEmployees } from "../app/helpers";
-import { getAllEmployees } from "../app/api";
-import { useDispatch } from "react-redux";
-import { getUsers } from "../features/user/UserDataSlice";
 
 const LandingPage = () => {
   const [userData, setUserData] = useState({
@@ -14,7 +9,6 @@ const LandingPage = () => {
   });
 
   const navigate = useNavigate();
-  const dispatch = useDispatch();
 
   // *  Handling submit trigger for  form submit
 
@@ -23,10 +17,6 @@ const LandingPage = () => {
     // login
     await login(userData);
     localStorage.setItem("token", "isLogged");
-    const employeesList = await getAllEmployees();
-    const filteredEmployeeObj = filterEmployees(employeesList);
-    // * Checking if localstorage token is present, and then only dispatching the filtered array
-    checkLocalStorage() && dispatch(getUsers(filteredEmployeeObj));
 
     navigate("/dashboard");
   };
